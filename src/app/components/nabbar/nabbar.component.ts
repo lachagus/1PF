36d3e2de8cf1a92394f1d28ffd069f171a3730cc
lachagus <chagusbigand@gmail.com>
2022-04-11 21:16:11 -0300

    Comienzo

---
+++ src/app/components/nabbar/nabbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nabbar',
+  templateUrl: './nabbar.component.html',
+  styleUrls: ['./nabbar.component.css']
+})
+export class NabbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
